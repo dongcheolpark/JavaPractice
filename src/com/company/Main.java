e79b989d49546d19d74e15cb991918ce2ed23ed3
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-02-14 21:21:29 +0900

    Practice1 상속

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ import com.company.practice1.MainPractice1;
 
 public class Main {
     public static void main(String[] args) {
-        MainPractice1._main(args);
+        MainPracticeInterface Run = new MainPractice1();
+        Run._main(args);
     }
 }
--- src/com/company/practice1/MainPractice1.java
+++ src/com/company/practice1/MainPractice1.java
@@ -1,9 +1,14 @@
 package com.company.practice1;
+import com.company.MainPracticeInterface;
 
-public class MainPractice1 {
-    public static void _main(String[] args) {
+public class MainPractice1 implements MainPracticeInterface { //클래스, 상속 연습
+    public void _main(String[] args) {
         Person myPerson = new Person("John");
         System.out.println(myPerson.getName());
         System.out.println(Person.getSpecies());
+
+        Student Joe = new Student("Joe",4.0);
+        System.out.println(Joe.getName());
+        System.out.println(Joe.getGpa());
     }
 }
